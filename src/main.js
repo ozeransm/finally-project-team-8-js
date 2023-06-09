4c9af6634eeefb6b01d65803228d0b86e6e323ae
Serhii Ozeran <ozeransm@gmail.com>
2023-06-09 18:14:03 +0300

    update task, change card-list to one random-card

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_GENRE_URL = 'https://api.themoviedb.org/3/genre/movie/list';
-const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500/';
+const BASE_IMG_URL = 'https://image.tmdb.org/t/p/original/';
 const BAES_UPCOMING_URL = 'https://api.themoviedb.org/3/movie/upcoming';
 const API_KEY = '057e36269a3ddafbb398756699f3ba82';
 const refs = {
@@ -16,7 +16,7 @@ async function responseData(url=''){
     return await axios.get(url);
     }
 }
-
+//https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&release_date.gte=2023-06-01&release_date.lte=2023-07-01 
 responseData(`${BAES_UPCOMING_URL}?api_key=${API_KEY}&language=en-US&page=1`)
 .then(markUp);
 
@@ -30,11 +30,14 @@ async function genreStr(arr){
 
 async function markUp(data){
     
-    data.data.results.map(async ({poster_path ,original_title, id, release_date, vote_average, vote_count, popularity, overview, genre_ids})=>{
-        
-        const str = `<div class="item"><img src="${BASE_IMG_URL}${poster_path}" alt="${original_title}"><div><h1>${original_title}</h1><h2>Release date ${release_date}</h2><h2>Vote / Votes ${vote_average} / ${vote_count }</h2><h2>Popularity ${popularity}</h2><h2 class="genre">${await genreStr(genre_ids)}</h2><p>About ${overview}</p></div></div>`;
-        refs.galery.insertAdjacentHTML('beforeend', str);
-        
+    const randCard = Math.floor(Math.random() * 21);
+    data.data.results.map(async ({backdrop_path ,original_title, id, release_date, vote_average, vote_count, popularity, overview, genre_ids},i)=>{
+        console.log(i)
+        if (i===randCard)
+        {
+            const str = `<div class="item"><img src="${BASE_IMG_URL}${backdrop_path}" alt="${original_title}"><div><h1>${original_title}</h1><h2>Release date ${release_date}</h2><h2>Vote / Votes ${vote_average} / ${vote_count }</h2><h2>Popularity ${popularity}</h2><h2 class="genre">${await genreStr(genre_ids)}</h2><p>About ${overview}</p></div></div>`;
+            refs.galery.insertAdjacentHTML('beforeend', str);
+        }
     });
 
     
